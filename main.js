3363acf90571052d96d0b6a8a7f09ea5e7f71f5f
Heathcliff <dennis.nathan@gmail.com>
2016-10-31 13:00:32 -0600

    Created makeTag function in main.js, all example json runs now.

--- main.js
+++ main.js
@@ -16,3 +16,28 @@ function onChange(event) {
     makeTag(null, null, data);
   }
 }
+
+function makeTag(tag, content, data) {
+  if (data) {
+    data = JSON.parse(data);
+    data.forEach((element) => {
+      makeTag(element.tag, element.content);
+    })
+  } else {
+    let newTag = `<${tag}>`;
+    if (Array.isArray(content)) {
+      content.forEach((element) => {
+        makeTag(element.tag, element.content)
+      })
+    }
+    if (content.tag) {
+      makeTag(content.tag, content.content);
+    } else {
+      if (typeof content === 'string') {
+        newTag += content;
+      }
+    }
+    newTag += `</${tag}>`;
+    $('#visualizer').append(newTag);
+  }
+}
