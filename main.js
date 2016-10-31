1afb2e15b2a4ebe55f993382c3176e763fb2d946
Heathcliff <dennis.nathan@gmail.com>
2016-10-31 12:59:45 -0600

    Set up filereader in main.js to return data from file upload

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+var control = document.getElementById('inputFile');
+control.addEventListener('change', function(event) {
+  onChange(event);
+}, false);
 
+function onChange(event) {
+  var data;
+  var file = event.target.files[0];
+  var reader = new FileReader();
+  reader.onload = function(event) {
+    data = event.target.result;
+  };
+
+  reader.readAsText(file);
+  reader.onloadend = () => {
+    makeTag(null, null, data);
+  }
+}
